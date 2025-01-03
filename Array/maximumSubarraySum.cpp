e12487be9e3277ff3753cbce5dc5a3171536c153
gauri Sonawane <gaurisonawane918@gmail.com>
2025-01-04 04:57:30 +0530

    array

--- Array/maximumSubarraySum.cpp
+++ Array/maximumSubarraySum.cpp
@@ -1,1 +1,25 @@
 //kadanes algorithm
+/*approach: take variable sum = 0
+varible max which contains one max element initially max=a[0]
+increment sum by adding next element to it
+find the maximum between maximum and sum and store it into maximum
+if sum<0 take sum = 0*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+    int maxSubArray(vector<int>& nums){
+        int sum = 0;
+        int maxi = nums[0];
+        for(int i = 0;i<nums.size();i++){
+            sum += nums[i];
+            maxi = max(sum,maxi);
+            if(sum<0) sum =0;
+
+        }
+        return maxi;
+    }
+    
+};
