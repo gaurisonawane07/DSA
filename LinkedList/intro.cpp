c5f40864e7f7eb8b130d81b2c7bd2a40150ca93f
gauri Sonawane <gaurisonawane918@gmail.com>
2025-04-01 23:06:46 +0530

    Revised

---
+++ LinkedList/intro.cpp
@@ -0,0 +1,7 @@
+//Linked list is a data structure which helps to increase as well as decrease the size
+//elemements are not in contigous location (location one after another)
+//size of the linked list can be increased and decreased at any moment
+//the starting point of the linked list is the head of the linked list
+//end point is konwn as tail of the linked list and its next pointer is null
+
+//it is used in stack,queue
