55b1d188c12e2d5891c6ff9d1e010d0972bc02a2
gauri Sonawane <gaurisonawane918@gmail.com>
2025-07-12 14:35:12 +0530

    Arrays

--- Array/Longest_Consecutive_Sequence.cpp
+++ Array/Longest_Consecutive_Sequence.cpp
@@ -5,6 +5,41 @@ Note:
 You can reorder the array to form a sequence.*/
 
 //Brute force approach: staart iterating from first element anf find its consecutive next then start from second element and fine its consecutive next
+
+//Better solution:
+/*
+first we have to sort the entire array
+cnt = 0;
+longest = 1;
+last_smaller = int_min;
+iterate through the array if the last_smaller is arr[i]-1;
+then add the element to the consecutive sequence update cnt,longest and last_smaller
+if (last_smaller != arr[i]) again start from fresh cnt  = 0;longest = 1;
+*/
+
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+int longestSuccessiveElements(vector<int>&a){
+    if(a.size() == 0) return 0;
+    sort(a.begin(),a.end());
+    int cnt  = 0;
+    int longest = 1;
+    int last_smaller = INT_MIN;
+    for(int i = 0;i<a.size();i++){
+        if(a[i]-1 == last_smaller){
+            cnt+=1;
+            last_smaller = a[i];
+        }
+        else if(a[i] != last_smaller){
+            cnt= 1;
+            last_smaller = a[i];
+        }
+        longest = max(longest,cnt);
+    }
+    return longest;
+}
 /*OPTIMAL SOLUTION: 
 1.use an unordered set to store all elements of the array for 0(1) lookup
 2.for each element,check if it is a start of a sequence(i.e. it-1 is not in the set)
