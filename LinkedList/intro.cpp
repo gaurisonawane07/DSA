5e0f3e3fd86abe52b48ad708ad6e06415fa29268
gauri Sonawane <gaurisonawane918@gmail.com>
2025-10-28 07:40:04 +0530

    basics of linked list

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -5,3 +5,29 @@
 //end point is konwn as tail of the linked list and its next pointer is null
 
 //it is used in stack,queue
+
+#include<iostream>
+#include<vector>
+using namespace std;
+struct Node{ //self define datatype for node to store data and next pointer
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){ //constructor
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){ //constructor
+        data = data1;
+        next = nullptr;
+    }
+};
+int main() {
+    vector<int> arr = {2, 5, 8, 7};
+    Node*y = new Node(arr[0]);
+    cout<<y;
+}
